daf7077299f300889d29ab15cc15fe3ba5367f95
Paul Gottschling <paul.gottschling@goteleport.com>
2025-03-07 11:02:06 -0500

    Fix bug
    With no version path, the function adds an erroneous leading slash.

--- src/theme/DocCardList/href-to-id.test.ts
+++ src/theme/DocCardList/href-to-id.test.ts
@@ -14,6 +14,11 @@ describe("categoryHrefToDocID", () => {
       href: "/ver/15.x/reference/agent-services/",
       expectedID: "reference/agent-services/agent-services",
     },
+    {
+      description: "category index page in unversioned path",
+      href: "/reference/agent-services/",
+      expectedID: "reference/agent-services/agent-services",
+    },
   ];
 
   test.each(testCases)("$description", (c) => {
--- src/theme/DocCardList/href-to-id.ts
+++ src/theme/DocCardList/href-to-id.ts
@@ -1,13 +1,9 @@
 // categoryHrefoToDocID returns the Docusaurus page ID that corresponds to the
-// given href. Category pages do not have IDs in the items prop, so we generate
-// a page ID based on the assumption that category page slugs are the same as
-// their containing directory names.
+// given href, which points to a category index page. Category pages do not have
+// IDs in the items prop, so we generate a page ID based on the assumption that
+// category page slugs are the same as their containing directory names.
 export function categoryHrefToDocID(href: string): string {
-  console.log("calling categoryHrefToDocID with:", href);
-  if (!href) {
-    return href;
-  }
-  const idPrefix = href.replace(new RegExp(`^/ver/[0-9]+\\.x/`), "");
+  const idPrefix = href.replace(new RegExp(`^(/ver/[0-9]+\\.x)?/`), "");
   const slugRE = new RegExp(`/([^/]+)/$`);
   const slug = slugRE.exec(href);
   if (!slug || slug.length != 2) {
