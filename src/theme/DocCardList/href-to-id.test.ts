599e460ab3b2d7f21442e4e78576433778059e3b
Paul Gottschling <paul.gottschling@goteleport.com>
2025-03-07 10:51:12 -0500

    Add passing test case

--- src/theme/DocCardList/href-to-id.test.ts
+++ src/theme/DocCardList/href-to-id.test.ts
@@ -8,7 +8,13 @@ describe("categoryHrefToDocID", () => {
     expectedID: string;
   }
 
-  const testCases: Array<testCase> = [];
+  const testCases: Array<testCase> = [
+    {
+      description: "category index page in versioned path",
+      href: "/ver/15.x/reference/agent-services/",
+      expectedID: "reference/agent-services/agent-services",
+    },
+  ];
 
   test.each(testCases)("$description", (c) => {
     expect(categoryHrefToDocID(c.href)).toEqual(c.expectedID);
